5dd980a69ad2d4371942c3688817cb6069318fd5
shubhamkgarg06 <shubham.k.garg@gmail.com>
2026-05-21 12:37:15 +0530

    Changes Folder structure and started frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Navbar from './components/navbar'
+
+function App() {
+
+  return (
+
+      <div className='bg-black-1000 min-h-screen'>
+        <Navbar />
+        <div>
+          <h1 className="text-4xltext-center mt-10">Welcome to My Website</h1>
+        </div>
+      </div>
+
+    
+  )
+}
+
+export default App
